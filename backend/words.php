bb3e5ea3254b73e5c746a1f98d7bc17b8a48ecc0
Dominik <rettigdk@gmail.com>
2021-07-15 18:30:22 +0200

    add php file

--- backend/words.php
+++ backend/words.php
@@ -1,18 +1,36 @@
 <?php
 $servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "users";
+$username = "root";
+$password = "";
+$dbname     = "words";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-    $trp = mysqli_query($conn, "SELECT * from userdetails");
-    $rows = array();
-    while($r = mysqli_fetch_assoc($trp)) {
-        $rows[] = $r;
-    }
-    print json_encode($rows); //convert php data to json data
-?>
+}
+$query = "SELECT * FROM words";
+$trp = mysqli_query($conn, $query);
+$rows = array();
+while ($r = mysqli_fetch_assoc($trp)) {
+    $rows[] = $r;
+}
+
+/**
+ *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
+ *  origin.
+ *
+ *  In a production environment, you probably want to be more restrictive, but this gives you
+ *  the general idea of what is involved.  For the nitty-gritty low-down, read:
+ *
+ *  - https://developer.mozilla.org/en/HTTP_access_control
+ *  - https://fetch.spec.whatwg.org/#http-cors-protocol
+ *
+ */
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+
+echo json_encode($rows);
